622739ff773b351c4693165a71ecf24d03bd57a5
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2025-01-20 14:29:05 +0530

    second  commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = require('./router/index.router')
 const app = express();
-
+app.set('view engine', 'ejs');
 app.use('/',router)
 
 module.exports = app
